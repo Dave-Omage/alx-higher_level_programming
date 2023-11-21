e78d4a66cfb074ad3c8bc21a64469f9176562405
Dave <davillamags@gmail.com>
2023-11-20 18:10:44 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that demonstrates:
+# how to print an integer
+# how to use a try ... except for exception handling
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
