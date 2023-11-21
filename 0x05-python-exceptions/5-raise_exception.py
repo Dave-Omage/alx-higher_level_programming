221bee88d8d12e873095c44ec19bcca750e7af77
Dave <davillamags@gmail.com>
2023-11-20 18:09:30 -0800

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a type exception error
+
+
+def raise_exception():
+    raise TypeError
