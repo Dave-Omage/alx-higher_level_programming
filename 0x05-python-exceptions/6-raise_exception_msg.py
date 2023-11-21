89955daf0a1b950ae2e337ad90f84fc507c48df5
Dave <davillamags@gmail.com>
2023-11-20 18:10:03 -0800

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a name exception with a message
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
