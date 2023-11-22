35b03c1b57a9eacb57fc60f8f9776f56e920f69f
Dave <davillamags@gmail.com>
2023-11-21 18:25:15 -0800

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # -----------------------------------------------------------
+"""Square Class.
 
+This module contains a class that defines a square.
+"""
 
 class Square:
     """Defines the blueprint of a square.
